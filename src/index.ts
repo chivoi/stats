c4b71207aea5e5a328a1f332a004eb74becf6a65
Ana Lastoviria <ana.lastoviria@zendesk.com>
2021-12-13 17:02:36 +1100

    refactor programme with DataReader interface

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
+import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
 
-const reader = new MatchReader('football.csv');
-reader.read();
-
-// const dateOfFirstMatch = reader.data[0][0];
+const csvFileReader = new CsvFileReader('football.csv');
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) manUnitedWins++;
     if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) manUnitedWins++;
 };
--- src/inheritance/CsvFileReader.ts
+++ src/inheritance/CsvFileReader.ts
@@ -1,8 +1,10 @@
 import fs from 'fs';
-export class CsvFileReader {
-    data: string[][]=[];
+export abstract class CsvFileReader<T> {
+    data: T[]=[];
     
     constructor(public filename: string){};
+
+    abstract mapRow(row: string[]): T;
 
     read(): void {
         this.data = fs.readFileSync(this.filename, {
@@ -11,5 +13,6 @@ export class CsvFileReader {
           .map((row: string): string[] => {
             return row.split(',');
         })
+          .map(this.mapRow);
     };
 };
